67e4635639295d178f528d437debae844e5fa12a
Kelvin <kelvingiovanno2000@gmail.com>
2026-02-27 14:20:55 +0700

    final

--- src/modules/employee/employee.controller.ts
+++ src/modules/employee/employee.controller.ts
@@ -1,17 +1,46 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseUUIDPipe,
+    Patch,
+    Post,
+} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
+import { CreateEmployeeDto } from './dto/createEmployee.dto';
+import { UpdateEmployeeDto } from './dto/updateEmployee.dto';
 
-@Controller('/employee')
+@Controller('/employees')
 export class EmployeeController {
     constructor(private readonly employeeService: EmployeeService) {}
 
-    @Get('/:username')
-    getUserByusername(@Param('username') username: string) {
-        return { name: 'kelvin', username: username };
+    @Get()
+    async getEmployee() {
+        return this.employeeService.getAllEmployee();
     }
 
-    @Get('/')
-    getUsers() {
-        return this.employeeService.getAllEmployee();
+    @Get('/:id')
+    async getEmployeeById(@Param('id', new ParseUUIDPipe()) id: string) {
+        return this.employeeService.getEmployeeById(id);
+    }
+
+    @Post()
+    async createEmployee(@Body() createEmployeeDto: CreateEmployeeDto) {
+        return this.employeeService.createEmployee(createEmployeeDto);
+    }
+
+    @Patch('/:id')
+    async updateEmployee(
+        @Param('id', new ParseUUIDPipe()) id: string,
+        @Body() updateEmployeeDto: UpdateEmployeeDto,
+    ) {
+        return this.employeeService.updateEmployee(id, updateEmployeeDto);
+    }
+
+    @Delete('/:id')
+    async deleteEmployee(@Param('id', new ParseUUIDPipe()) id: string) {
+        return this.employeeService.deleteEmployee(id);
     }
 }
--- src/modules/employee/employee.service.ts
+++ src/modules/employee/employee.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Employee } from 'generated/prisma/client';
+import {
+    EmployeeCreateInput,
+    EmployeeUpdateInput,
+} from 'generated/prisma/models';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -8,5 +12,51 @@ export class EmployeeService {
 
     async getAllEmployee(): Promise<Employee[] | null> {
         return await this.prisma.employee.findMany();
+    }
+
+    async getEmployeeById(id: string): Promise<Employee> {
+        const employee = await this.prisma.employee.findUnique({
+            where: { id: id },
+        });
+
+        if (!employee) {
+            throw new NotFoundException();
+        }
+
+        return employee;
+    }
+
+    async createEmployee(data: EmployeeCreateInput): Promise<Employee> {
+        return await this.prisma.employee.create({
+            data: data,
+        });
+    }
+
+    async updateEmployee(
+        id: string,
+        data: EmployeeUpdateInput,
+    ): Promise<Employee> {
+        const employee = await this.prisma.employee.update({
+            where: { id: id },
+            data: data,
+        });
+
+        if (!employee) {
+            throw new NotFoundException();
+        }
+
+        return employee;
+    }
+
+    async deleteEmployee(id: string): Promise<Employee | null> {
+        const employee = await this.prisma.employee.delete({
+            where: { id: id },
+        });
+
+        if (!employee) {
+            throw new NotFoundException();
+        }
+
+        return employee;
     }
 }
