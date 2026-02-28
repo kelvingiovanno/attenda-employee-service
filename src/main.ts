91ca2ca1c755642bdd85667b3be9a385d0aaf9c1
Kelvin <kelvingiovanno2000@gmail.com>
2026-02-28 12:59:19 +0700

    add tcp connection

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { rpcError } from './common/errors/rpc-error';
+import { ErrorCode } from './common/errors/error-code.enum';
 
 async function bootstrap() {
     const app = await NestFactory.createMicroservice<MicroserviceOptions>(
         AppModule,
         {
             transport: Transport.TCP,
+            options: {
+                host: 'localhost',
+                port: 8807,
+                retryAttempts: 3,
+            },
         },
     );
 
@@ -16,6 +23,15 @@ async function bootstrap() {
             whitelist: true,
             forbidNonWhitelisted: true,
             transform: true,
+            exceptionFactory(errors) {
+                rpcError({
+                    code: ErrorCode.VALIDATION_ERROR,
+                    message: 'Validation error.',
+                    errors: errors
+                        .map((e) => Object.values(e.constraints!))
+                        .flat(),
+                });
+            },
         }),
     );
 
